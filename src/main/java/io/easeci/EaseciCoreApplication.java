ce01ab69aaabe9824867e847121c58cd170f2d00
meksula <meksula.karol.93@gmail.com>
2020-01-26 21:57:25 +0100

    Workspace initializing implementation added

--- src/main/java/io/easeci/EaseciCoreApplication.java
+++ src/main/java/io/easeci/EaseciCoreApplication.java
@@ -1,14 +1,26 @@
 package io.easeci;
 
+import io.easeci.core.workspace.StandardWorkspaceInitializer;
+import io.easeci.core.workspace.Workspace;
 import io.easeci.interpreter.Python;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.nio.file.Path;
+import java.util.Optional;
+
 @SpringBootApplication
 public class EaseciCoreApplication {
+    private static Workspace workspace;
+
+    public static void prepare(String[] args) {
+        Python.initializeInterpreter();
+        EaseciCoreApplication.workspace = new StandardWorkspaceInitializer();
+        workspace.init(args.length > 0 ? Optional.of(Path.of(args[0])) : Optional.empty());
+    }
 
     public static void main(String[] args) {
-        Python.initializeInterpreter();
+        prepare(args);
         SpringApplication.run(EaseciCoreApplication.class, args);
     }
 
