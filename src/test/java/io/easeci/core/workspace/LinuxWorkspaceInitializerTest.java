d83ce5817b71eaab28ca6fc201ab69e80468b761
meksula <meksula.karol.93@gmail.com>
2020-02-04 21:07:56 +0100

    Workspace initializer for Linux OS unit test added

--- src/test/java/io/easeci/core/workspace/LinuxWorkspaceInitializerTest.java
+++ src/test/java/io/easeci/core/workspace/LinuxWorkspaceInitializerTest.java
@@ -1,7 +1,66 @@
 package io.easeci.core.workspace;
 
+import io.easeci.utils.io.YamlUtils;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import static io.easeci.core.workspace.LinuxWorkspaceInitializer.BOOTSTRAP_FILENAME;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinuxWorkspaceInitializerTest {
+    private LinuxWorkspaceInitializer workspaceInitializer;
 
+    private final Path TEST_WORKSPACE_PATH = Paths.get("/tmp/easeci_test");
+
+    @BeforeEach
+    void setup() {
+        this.workspaceInitializer = LinuxWorkspaceInitializer.getInstance();
+    }
+
+    @Test
+    @DisplayName("Should correctly initialize workspace in indicated directory")
+    void initTest() {
+        Optional<Path> optionalPath = Optional.of(TEST_WORKSPACE_PATH);
+        workspaceInitializer.init(optionalPath);
+
+        Path pwd = Path.of(System.getProperty("user.dir"));
+        Path runYmlPath = Paths.get(pwd.toString().concat("/").concat(BOOTSTRAP_FILENAME));
+
+        assertAll(() -> assertTrue(Files.exists(runYmlPath)),
+                () -> assertTrue(Files.exists(Paths.get(String.valueOf(YamlUtils.ymlGet(runYmlPath, "easeci.workspace.path").getValue())))));
+    }
+
+    @Test
+    @DisplayName("Should correctly initialize workspace in current direcory if Optional<Path> is empty")
+    void initEmptyTest() {
+        Optional<Path> optionalPath = Optional.empty();
+        workspaceInitializer.init(optionalPath);
+
+        Path pwd = Path.of(System.getProperty("user.dir"));
+        Path runYmlPath = Paths.get(pwd.toString().concat("/").concat(BOOTSTRAP_FILENAME));
+
+        assertAll(() -> assertTrue(Files.exists(runYmlPath)),
+                () -> assertTrue(Files.exists(Paths.get(String.valueOf(YamlUtils.ymlGet(runYmlPath, "easeci.workspace.path").getValue())))));
+    }
+
+    void deleteRunYaml() throws IOException {
+        Path pwd = Path.of(System.getProperty("user.dir"));
+        Path runYmlPath = Paths.get(pwd.toString().concat("/").concat(BOOTSTRAP_FILENAME));
+        Files.deleteIfExists(runYmlPath);
+    }
+
+    @AfterEach
+    void cleanup() throws IOException {
+        FileUtils.deleteDirectory(TEST_WORKSPACE_PATH.toFile());
+        deleteRunYaml();
+    }
 }
