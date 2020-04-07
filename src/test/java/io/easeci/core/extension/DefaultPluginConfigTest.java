e20c204ed4d140160851403bad6938f302601f1d
meksula <meksula.karol.93@gmail.com>
2020-04-07 22:43:06 +0200

    Test yaml files added, null pointer checking methods added, and tests added - need implementation

---
+++ src/test/java/io/easeci/core/extension/DefaultPluginConfigTest.java
@@ -0,0 +1,57 @@
+package io.easeci.core.extension;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefaultPluginConfigTest {
+
+    @Test
+    @DisplayName("Should correctly instantiate object")
+    void instantiateTest() {
+
+    }
+
+    @Test
+    @DisplayName("Should correctly load config from file indicated as a method argument")
+    void loadTest() {
+
+    }
+
+    @Test
+    @DisplayName("Should throws IllegalStateException when trying to construct object with null argument")
+    void instantiateInvalidTest() {
+
+    }
+
+    @Test
+    @DisplayName("Should return null when exception occurred while config file parsing")
+    void loadReturnNull() {
+
+    }
+
+    @Test
+    @DisplayName("Should return null when one of method argument is null")
+    void loadReturnNullArgumentNull() {
+
+    }
+
+    @Test
+    @DisplayName("Should return null when list in method argument is empty")
+    void loadReturnNullEmptyList() {
+
+    }
+
+    @Test
+    @DisplayName("Should return one and only instance when this instance is on list in method argument")
+    void loadReturnOneInstanceOnList() {
+
+    }
+
+    @Test
+    @DisplayName("Should return correctly instance marked in field `Boolean enabled` as true")
+    void loadReturnInstanceMarkedAsTrue() {
+
+    }
+}
--- src/test/java/io/easeci/core/extension/PluginsConfigFileTest.java
+++ src/test/java/io/easeci/core/extension/PluginsConfigFileTest.java
@@ -1,8 +1,10 @@
 package io.easeci.core.extension;
 
 import commons.WorkspaceTestUtils;
+import io.easeci.utils.io.YamlUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.ThrowingSupplier;
 
 import java.util.List;
 import java.util.Map;
@@ -11,7 +13,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PluginsConfigFileTest {
     private final static String FILE_CORRECT = "workspace/plugins-config-test.yml",
-                              FILE_INCORRECT = "workspace/plugins-config-test-invalid.yml";
+                              FILE_INCORRECT = "workspace/plugins-config-test-invalid.yml",
+                                  FILE_EMPTY = "workspace/plugins-config-test-empty.yml";
 
     @Test
     @DisplayName("Should correctly create PluginConfigFile and correctly parse provided map")
@@ -27,5 +30,29 @@ class PluginsConfigFileTest {
         assertEquals(2, size);
         assertEquals(2, configDescriptionListA.size());
         assertEquals(2, configDescriptionListB.size());
+    }
+
+    @Test
+    @DisplayName("Should not throwing and not return null pointer when trying to load empty config from plugins-config.yml file")
+    void loadConfigFromFileTest() {
+        Map<?, ?> values = WorkspaceTestUtils.loadYamlFromResources(FILE_EMPTY);
+        PluginsConfigFile pluginsConfigFile = PluginsConfigFile.of(values);
+
+        assertDoesNotThrow((ThrowingSupplier<NullPointerException>) NullPointerException::new);
+        assertNotNull(pluginsConfigFile);
+    }
+
+    @Test
+    @DisplayName("Should not throw NullPointerException when method's argument is null, should throw YamlException")
+    void loadConfigWithNullTest() {
+        assertThrows(YamlUtils.YamlException.class, () -> PluginsConfigFile.of(null));
+    }
+
+    @Test
+    @DisplayName("Should throw ClassCastException when data in file is malformed (not expected types)")
+    void loadConfigFromMalformedFileTest() {
+        Map<?, ?> values = WorkspaceTestUtils.loadYamlFromResources(FILE_INCORRECT);
+
+        assertThrows(ClassCastException.class, () -> PluginsConfigFile.of(values));
     }
 }
