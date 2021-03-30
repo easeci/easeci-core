de2a7120c410208e67d1963c58e7ee22306ae34d
meksula <meksula.karol.93@gmail.com>
2021-03-30 21:26:26 +0200

    [core #0031] Exceptions throwing in pipeline parsing module is not propagated to http response

--- src/main/java/io/easeci/api/parsing/ParseProcessResponse.java
+++ src/main/java/io/easeci/api/parsing/ParseProcessResponse.java
@@ -1,15 +1,21 @@
 package io.easeci.api.parsing;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.easeci.core.engine.easefile.parser.EasefileParseResult;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ParseProcessResponse {
     private Boolean isSuccessfullyDone;
     private String message;
+    private EasefileParseResult easefileParseResult;
 
     public static ParseProcessResponse of(EasefileParseResult easefileParseResult) {
-        return null;
+        ParseProcessResponse response = new ParseProcessResponse();
+        response.setIsSuccessfullyDone(easefileParseResult.isSuccess());
+        response.setEasefileParseResult(easefileParseResult);
+        return response;
     }
 
     public static ParseProcessResponse withError(String exceptionMessage) {
